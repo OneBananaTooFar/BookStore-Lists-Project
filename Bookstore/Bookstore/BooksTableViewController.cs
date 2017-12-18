b2b4a083557a96793dce7f6235266623be7f9587
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-18 13:25:27 -0700

    Initial commit

---
+++ Bookstore/Bookstore/BooksTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Bookstore
+{
+    public partial class BooksTableViewController : UITableViewController
+    {
+        public BooksTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# BookStore-Lists-Project
