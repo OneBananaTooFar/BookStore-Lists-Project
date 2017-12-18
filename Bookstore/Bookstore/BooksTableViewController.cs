9605f7cfea0c14d236ce691e6b8d6ea97265e9cc
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-18 13:35:04 -0700

    BookStoreProject

--- Bookstore/Bookstore/BooksTableViewController.cs
+++ Bookstore/Bookstore/BooksTableViewController.cs
@@ -1,13 +1,27 @@
 using Foundation;
 using System;
 using UIKit;
+using System.Collections.Generic;
 
 namespace Bookstore
 {
     public partial class BooksTableViewController : UITableViewController
     {
+        List<Book> BookList;
+
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
+            BookList = new List<Book>();
         }
     }
+
+    public class Book
+    {
+        public string Name;
+        public string Author;
+        public string Editor;
+        public int Year;
+
+    }
+
 }
