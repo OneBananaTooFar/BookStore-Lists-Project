e060b474033d9f481e5114d2dd37bf5a73cf63d7
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-18 14:01:43 -0700

    Update

--- Bookstore/Bookstore/BooksTableViewController.cs
+++ Bookstore/Bookstore/BooksTableViewController.cs
@@ -7,21 +7,74 @@ namespace Bookstore
 {
     public partial class BooksTableViewController : UITableViewController
     {
-        List<Book> BookList;
+        List<Book> bookList;
 
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
-            BookList = new List<Book>();
+            bookList = new List<Book>();
+
+            bookList.Add(new Book()
+            {
+                Author = "J.K. Rowling",
+                Name = "Harry Potter and the Sorcerer's Stone",
+                Publisher = "Bloomsbury",
+                Year = 1997
+            });
+            bookList.Add(new Book()
+            {
+                Author = "J.K Rowling",
+                Name = "Harry Potter and the Chamber of Secets",
+                Publisher = "Bloomsbury",
+                Year = 1998
+            });
+
+            bookList.Add(new Book()
+            {
+                Author = "J.K. Rowling",
+                Name = "Harry Potter and the Prisoner of Azkaban",
+                Publisher = "Bloomsbury",
+                Year = 1999
+            });
+
         }
-    }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return bookList.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("Book");
+
+            var data = bookList[indexPath.Row];
+
+            cell.TextLabel.Text = data.Name;
+
+            return cell;
+        }
+    
+        }
+       
+        }
+
+
+
+
+
 
     public class Book
     {
         public string Name;
         public string Author;
-        public string Editor;
+        public string Publisher;
         public int Year;
 
     }
 
-}
+lllll
