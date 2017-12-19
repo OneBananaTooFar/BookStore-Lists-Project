87c4ca34802c4b395e2897a1c4eb5ed253b032c2
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-19 12:44:58 -0700

    K

--- Bookstore/Bookstore/BooksTableViewController.cs
+++ Bookstore/Bookstore/BooksTableViewController.cs
@@ -57,7 +57,7 @@ namespace Bookstore
             cell.TextLabel.Text = data.Name;
 
             return cell;
-        }kk
+        }
     
         }
        
@@ -77,4 +77,3 @@ namespace Bookstore
 
     }
 
-k
